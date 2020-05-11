b07e51cbc954dc1995f13325720c8d1ed28e079b
Minh Nhat Ta <taminhnhat001@gmail.com>
2020-05-11 17:03:08 +0700

    Update GoogleMaps

--- TheoDoiMoiTruong/GoogleMaps.swift
+++ TheoDoiMoiTruong/GoogleMaps.swift
@@ -10,13 +10,32 @@ import UIKit
 import GoogleMaps
 import GooglePlaces
 
+class TrackDestination: NSObject {
+    let name: String
+    let location: CLLocationCoordinate2D
+    let aqi: String
+    let zoom: Float
+    
+    init (name: String, location: CLLocationCoordinate2D, aqi: String, zoom: Float) {
+        self.name = name
+        self.location = location
+        self.aqi = aqi
+        self.zoom = zoom
+    }
+}
+
 class GoogleMaps: UIViewController {
+    
     var mapView:GMSMapView?
+    
+    var currentDestination: TrackDestination?
+    
+    var destinations:[TrackDestination] = [TrackDestination(name: "KDT Kim Văn Kim Lũ", location: CLLocationCoordinate2DMake(20.972998, 105.820044), aqi: "137 AQI", zoom: 18),                                      TrackDestination(name: "Royal City", location: CLLocationCoordinate2DMake(21.001763, 105.815960), aqi: "145 AQI", zoom: 15)]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let camera = GMSCameraPosition.camera(withLatitude: 20.976106, longitude: 105.815626, zoom: 10)
+        let camera = GMSCameraPosition.camera(withLatitude: 20.976106, longitude: 105.815626, zoom: 15)
         mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         view = mapView
         
@@ -24,16 +43,40 @@ class GoogleMaps: UIViewController {
         let marker = GMSMarker(position: currentLocation)
         marker.title = "Thang Long University"
         marker.map = mapView
+        marker.snippet = "124 AQI"
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .plain, target: self, action: #selector(nextL))
     
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        destinations.append(TrackDestination(name: "Thang Long University", location: CLLocationCoordinate2DMake(20.976096, 105.815593), aqi: "124 AQI", zoom: 15))
+    }
+    
     @objc func nextL() {
-        let nextLocation = CLLocationCoordinate2DMake(21.001763, 105.815960)
-        mapView?.camera = GMSCameraPosition.camera(withLatitude: nextLocation.latitude, longitude: nextLocation.longitude, zoom: 15)
-        let marker = GMSMarker(position: nextLocation)
-        marker.title = "Royal City"
+        
+        if currentDestination == nil {
+            currentDestination =  destinations.first
+        } else {
+            if let index = destinations.index(of: currentDestination!) {
+                currentDestination = destinations[(index + 1) % destinations.count]
+            }
+        }
+        
+        setMapCamera()
+    }
+    
+    private func setMapCamera() {
+        CATransaction.begin()
+        CATransaction.setValue(2, forKey: kCATransactionAnimationDuration)
+        
+        mapView?.animate(to: GMSCameraPosition.camera(withTarget: currentDestination!.location, zoom: currentDestination!.zoom))
+        
+        CATransaction.commit()
+        
+        let marker = GMSMarker(position: currentDestination!.location)
+        marker.title = currentDestination!.name
+        marker.snippet = currentDestination!.aqi
         marker.map = mapView
     }
 
