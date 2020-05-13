274462d08336cdf2e24729662acce32f7dc3e06b
Minh Nhat Ta <taminhnhat001@gmail.com>
2020-05-13 13:44:00 +0700

    add Lịch sử NGÀY

--- TheoDoiMoiTruong/DetailRP/DetailReport.swift
+++ TheoDoiMoiTruong/DetailRP/DetailReport.swift
@@ -11,8 +11,12 @@ import Charts
 
 class DetailReport: UIViewController {
 
-    var gioArr:[String] = []
-    var valueArr:[Double] = []
+    var gioArr:[String] = [] //Mảng lưu các giá trị GIỜ (Trục x)
+    var valueGioArr:[Double] = [] //Mảng lưu thông số của giá trị theo giờ (Trục y)
+    var dateArr:[String] = [] //Mảng lưu các giá trị NGÀY (Trục x)
+    var valueDateArr:[Double] = [] //Mảng lưu thông số của giá trị theo ngày (Trục y)
+    var isHourBarSelected:Bool = true //Biến kiểm tra biểu đồ GIỜ được chọn
+    var isDateBarSelected:Bool = false //Biến kiểm tra biểu đồ NGÀY được chọn
     
     @IBOutlet weak var myCollectionView: UICollectionView!
     @IBOutlet weak var myBarChartView: BarChartView!
@@ -43,17 +47,27 @@ class DetailReport: UIViewController {
         lblNgay.layer.borderWidth = 1.0
         lblNgay.layer.cornerRadius = 5
         lblNgay.clipsToBounds = true
-        //Set gia tri cho mang
+        //Set giá trị cho mảng Hour
         gioArr = ["01:00","02:00","03:00","04:00","05:00","06:00",
                   "07:00","08:00","09:00","10:00","11:00","12:00",
                   "13:00","14:00","15:00","16:00","17:00","18:00",
                   "19:00","20:00","21:00","22:00","23:00","24:00"]
-        valueArr = [110,110,125,120,146,137,53,51,62,123,62,91,33,167,110,85,92,44,120,95,104,107,132,165]
+        valueGioArr = [110,110,125,120,146,137,53,51,62,123,62,91,33,167,110,85,92,44,120,95,104,107,132,165]
+        //Set giá trị cho mảng Date
+        dateArr = ["1", "2", "3", "4", "5", "6",
+                   "7", "8", "9", "10", "11", "12",
+                   "13", "14", "15", "16", "17", "18",
+                   "19", "20", "21", "22", "23", "24",
+                   "25", "26", "27", "28", "29", "30", "31"]
+        valueDateArr = [85,92,110,33,42,152,94,18,120,110,45,32,67,83,92,95,167,152,124,145,124,34,45,62,71,89,120,167,129,130,141]
         
-        setBarChart(name: gioArr, value: valueArr)
+        isHourBarSelected = true
+        isDateBarSelected = false
+        setHourBarChart(name: gioArr, value: valueGioArr)
     }
     
-    func setBarChart(name:[String], value:[Double]) {
+    //Biểu đồ Hour
+    func setHourBarChart(name:[String], value:[Double]) {
         //Nếu không có dữ liệu thì:
         myBarChartView.noDataText = "Không có dữ liệu"
         //Hiện tên trục X theo tên giờ
@@ -65,16 +79,16 @@ class DetailReport: UIViewController {
             let data:BarChartDataEntry = BarChartDataEntry(x: Double(i), y: value[i])
             dataArr.append(data)
         }
-        var dataSet:BarChartDataSet = BarChartDataSet(entries: dataArr, label: "Chỉ số ô nhiễm")
+        let dataSet:BarChartDataSet = BarChartDataSet(entries: dataArr, label: "Chỉ số ô nhiễm")
         //Check giá trị rồi set màu tương ứng
-        dataSet.colors = [setColorBarChart(value: valueArr[0]), setColorBarChart(value: valueArr[1]), setColorBarChart(value: valueArr[2]),
-                          setColorBarChart(value: valueArr[3]), setColorBarChart(value: valueArr[4]), setColorBarChart(value: valueArr[5]),
-                          setColorBarChart(value: valueArr[6]), setColorBarChart(value: valueArr[7]), setColorBarChart(value: valueArr[8]),
-                          setColorBarChart(value: valueArr[9]), setColorBarChart(value: valueArr[10]), setColorBarChart(value: valueArr[11]),
-                          setColorBarChart(value: valueArr[12]), setColorBarChart(value: valueArr[13]), setColorBarChart(value: valueArr[14]),
-                          setColorBarChart(value: valueArr[15]), setColorBarChart(value: valueArr[16]), setColorBarChart(value: valueArr[17]),
-                          setColorBarChart(value: valueArr[18]), setColorBarChart(value: valueArr[19]), setColorBarChart(value: valueArr[20]),
-                          setColorBarChart(value: valueArr[21]), setColorBarChart(value: valueArr[22]), setColorBarChart(value: valueArr[23])]
+        dataSet.colors = [setColorBarChart(value: valueGioArr[0]), setColorBarChart(value: valueGioArr[1]), setColorBarChart(value: valueGioArr[2]),
+                          setColorBarChart(value: valueGioArr[3]), setColorBarChart(value: valueGioArr[4]), setColorBarChart(value: valueGioArr[5]),
+                          setColorBarChart(value: valueGioArr[6]), setColorBarChart(value: valueGioArr[7]), setColorBarChart(value: valueGioArr[8]),
+                          setColorBarChart(value: valueGioArr[9]), setColorBarChart(value: valueGioArr[10]), setColorBarChart(value: valueGioArr[11]),
+                          setColorBarChart(value: valueGioArr[12]), setColorBarChart(value: valueGioArr[13]), setColorBarChart(value: valueGioArr[14]),
+                          setColorBarChart(value: valueGioArr[15]), setColorBarChart(value: valueGioArr[16]), setColorBarChart(value: valueGioArr[17]),
+                          setColorBarChart(value: valueGioArr[18]), setColorBarChart(value: valueGioArr[19]), setColorBarChart(value: valueGioArr[20]),
+                          setColorBarChart(value: valueGioArr[21]), setColorBarChart(value: valueGioArr[22]), setColorBarChart(value: valueGioArr[23])]
         //Không hiện thông số trên mỗi bar
         dataSet.drawValuesEnabled = false
         //Ko hiện chú thích
@@ -85,11 +99,50 @@ class DetailReport: UIViewController {
         myBarChartView.rightAxis.drawGridLinesEnabled = false
         //Bỏ hiện giá trị bên phải
         myBarChartView.rightAxis.drawLabelsEnabled = false
-        //Chuyển các label giờ xuống dưới bott
+        //Chuyển các label giờ xuống dưới bot
         myBarChartView.xAxis.labelPosition = XAxis.LabelPosition.bottom
         
+        let dataChart:BarChartData = BarChartData(dataSet: dataSet)
+        myBarChartView.data = dataChart
+    }
+    
+    //Biểu đồ Date
+    func setDateBarChart(name:[String], value:[Double]) {
+        //Nếu không có dữ liệu thì:
+        myBarChartView.noDataText = "Không có dữ liệu"
+        //Hiện tên trục X theo tên giờ
+        myBarChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values: dateArr)
+        myBarChartView.xAxis.granularity = 1
         
-        var dataChart:BarChartData = BarChartData(dataSet: dataSet)
+        var dataArr:[BarChartDataEntry] = []
+        for i in 0 ..< dateArr.count {
+            let data:BarChartDataEntry = BarChartDataEntry(x: Double(i), y: value[i])
+            dataArr.append(data)
+        }
+        let dataSet:BarChartDataSet = BarChartDataSet(entries: dataArr, label: "Chỉ số ô nhiễm")
+        //Check giá trị rồi set màu tương ứng
+        dataSet.colors = [setColorBarChart(value: valueDateArr[0]), setColorBarChart(value: valueDateArr[1]), setColorBarChart(value: valueDateArr[2]),
+                          setColorBarChart(value: valueDateArr[3]), setColorBarChart(value: valueDateArr[4]), setColorBarChart(value: valueDateArr[5]),
+                          setColorBarChart(value: valueDateArr[6]), setColorBarChart(value: valueDateArr[7]), setColorBarChart(value: valueDateArr[8]),
+                          setColorBarChart(value: valueDateArr[9]), setColorBarChart(value: valueDateArr[10]), setColorBarChart(value: valueDateArr[11]),
+                          setColorBarChart(value: valueDateArr[12]), setColorBarChart(value: valueDateArr[13]), setColorBarChart(value: valueDateArr[14]),
+                          setColorBarChart(value: valueDateArr[15]), setColorBarChart(value: valueDateArr[16]), setColorBarChart(value: valueDateArr[17]),
+                          setColorBarChart(value: valueDateArr[18]), setColorBarChart(value: valueDateArr[19]), setColorBarChart(value: valueDateArr[20]),
+                          setColorBarChart(value: valueDateArr[21]), setColorBarChart(value: valueDateArr[22]), setColorBarChart(value: valueDateArr[23])]
+        //Không hiện thông số trên mỗi bar
+        dataSet.drawValuesEnabled = false
+        //Ko hiện chú thích
+        myBarChartView.legend.enabled = false
+        //Ko hiện đường kẻ background
+        myBarChartView.xAxis.drawGridLinesEnabled = false
+        myBarChartView.leftAxis.drawGridLinesEnabled = false
+        myBarChartView.rightAxis.drawGridLinesEnabled = false
+        //Bỏ hiện giá trị bên phải
+        myBarChartView.rightAxis.drawLabelsEnabled = false
+        //Chuyển các label giờ xuống dưới bot
+        myBarChartView.xAxis.labelPosition = XAxis.LabelPosition.bottom
+        
+        let dataChart:BarChartData = BarChartData(dataSet: dataSet)
         myBarChartView.data = dataChart
     }
     
@@ -144,22 +197,31 @@ class DetailReport: UIViewController {
     }
     
     //Action Lịch sử
+    //Chọn GIỜ
     @IBAction func actionGio(_ sender: UIButton) {
+        isHourBarSelected = true
+        isDateBarSelected = false
         lblGio.layer.borderColor = UIColor.darkText.cgColor
         lblNgay.layer.borderColor = UIColor.gray.cgColor
         lblGio.textColor = UIColor.white
         lblNgay.textColor = UIColor(red: 91/255, green: 155/255, blue: 213/255, alpha: 1) //Xanh nuoc bien nhat
         lblGio.backgroundColor = UIColor(red: 91/255, green: 155/255, blue: 213/255, alpha: 1) //Xanh nuoc bien nhat
         lblNgay.backgroundColor = UIColor.white
+        setHourBarChart(name: gioArr, value: valueGioArr)
+        myBarChartView.reloadInputViews()
     }
-    
+    //Chọn NGÀY
     @IBAction func actionNgay(_ sender: UIButton) {
+        isHourBarSelected = false
+        isDateBarSelected = true
         lblNgay.layer.borderColor = UIColor.darkText.cgColor
         lblGio.layer.borderColor = UIColor.gray.cgColor
         lblNgay.textColor = UIColor.white
         lblGio.textColor = UIColor(red: 91/255, green: 155/255, blue: 213/255, alpha: 1) //Xanh nuoc bien nhat
         lblNgay.backgroundColor = UIColor(red: 91/255, green: 155/255, blue: 213/255, alpha: 1) //Xanh nuoc bien nhat
         lblGio.backgroundColor = UIColor.white
+        setDateBarChart(name: dateArr, value: valueDateArr)
+        myBarChartView.reloadInputViews()
     }
 }
 
@@ -178,8 +240,15 @@ extension DetailReport: UICollectionViewDelegate, UICollectionViewDataSource, UI
 //Bấm vào cột trong biểu đồ hiển thị chỉ số trên label AQI
 extension DetailReport: ChartViewDelegate {
     func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
-        let pos = NSInteger(entry.x)
-        let valueInt:Int = Int(valueArr[pos])
-        lblAQI.text = String(valueInt)
+        if isHourBarSelected == true {
+            let pos = NSInteger(entry.x)
+            let valueInt:Int = Int(valueGioArr[pos])
+            lblAQI.text = String(valueInt)
+        }
+        if isDateBarSelected == true {
+            let pos = NSInteger(entry.x)
+            let valueInt:Int = Int(valueDateArr[pos])
+            lblAQI.text = String(valueInt)
+        }
     }
 }
